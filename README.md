432a8843dfd13f758e4a1dca5f3a73d042c38da7
PratilipiAich <pratilipiaich@gmail.com>
2020-06-12 17:08:36 +0530

    Added to-do app and README.md

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # To-Do-App
+
+A web based Task Manager/To-Do application using ReactJS with ReactHooks.
+
+## Features
+You can perform the following actions:
+	1. Add new tasks to the list
+	2. Delete any added task
+	3. Mark any task as done  
+
+## Note: Under progress!
---
+++ todo-app/src/components/TodoForm.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import uuid from 'uuid';
+import { Button, TextField } from '@material-ui/core';
+
+function TodoForm({ addTodo }) {
+
+    const [todo, setTodo] = useState({
+        id: "",
+        task: "",
+        completed: false
+    });
+
+    function handleTaskInputChange(e) {
+        setTodo({ ...todo, task: e.target.value});
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (todo.task.trim()) {
+            addTodo({ ...todo, id: uuid.v4() });
+            // Reset task input
+            setTodo({ ...todo, task: ""});
+        }
+    }
+    return (
+        
+        <form className="todo-form" onSubmit={handleSubmit}>
+            <TextField 
+            label="Task"
+            name="task"
+            type="text"
+            value={todo.task}
+            onChange={handleTaskInputChange}
+            />
+            <Button type="submit" style={{  
+                backgroundColor: '#282c34', 
+                fontFamily: "sans-serif",
+                border: 2, padding: 8, color: 'white' }}>add</Button>
+        </form>
+    );
+}
+
+export default TodoForm;
